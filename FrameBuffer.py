bf80b0e13707280a1844fda208abb1a48f76c7fc
Tanganelli <giacomo.tanganelli@for.unipi.it>
2014-05-27 17:14:46 +0200

    buttons

--- FrameBuffer.py
+++ FrameBuffer.py
@@ -1,14 +1,19 @@
-__author__ = 'jacko'
-
+import array
+import cairo
+import rsvg
 import os
 import pygame
-import time
+from pygame.constants import MOUSEBUTTONDOWN
+from button import buttons
+
+__author__ = 'jacko'
 
 
 class PyScope:
     screen = None
 
     def __init__(self):
+
         """Ininitializes a new pygame screen using the framebuffer"""
         # Based on "Python GUI in Linux frame buffer"
         # http://www.karoltomala.com/blog/?p=679
@@ -38,6 +43,7 @@ class PyScope:
         size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
         print "Framebuffer size: %d x %d" % (size[0], size[1])
         self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+        self.screen_size = size
         # Clear the screen to start
         self.screen.fill((0, 0, 0))
         # Initialise font support
@@ -45,18 +51,43 @@ class PyScope:
         # Render the screen
         pygame.display.update()
 
+
     def __del__(self):
         """Destructor to make sure pygame shuts down, etc."""
 
     def test(self):
         # Fill the screen with red (255, 0, 0)
-        red = (255, 0, 0)
+        red = (255, 255, 255)
         self.screen.fill(red)
-        scope.screen.fill((255, 255, 0))
         # Update the display
         pygame.display.update()
 
-# Create an instance of the PyScope class
-scope = PyScope()
-scope.test()
-time.sleep(10)
+    def display_image(self):
+        svg = rsvg.Handle(file="images/thermometer.svg")
+        width, height, dw, dh = svg.get_dimension_data()
+        data = array.array('c', chr(0) * width * height * 4)
+        surface = cairo.ImageSurface.create_for_data(
+            data, cairo.FORMAT_ARGB32, width, height, width * 4)
+
+        ctx = cairo.Context(surface)
+        screen_width = self.screen_size[0]
+        screen_height = self.screen_size[1]
+
+        factor_x = (screen_width / 100.0) * 10
+        factor_y = (screen_height / 100.0) * 10
+
+        factor = min(factor_x, factor_y)
+        ctx.scale(factor / width, factor / height)
+        svg.render_cairo(ctx)
+
+        image = pygame.image.frombuffer(data.tostring(), (width, height), "ARGB")
+        self.screen.blit(image, (10, 10))
+        # Render the screen
+        pygame.display.update()
+
+    def create_button(self):
+        button = buttons.Button()
+        #Parameters:               surface,      color,       x,   y,   length, height, width,    text,      text_color
+        button.create_button(self.screen, (107, 142, 35), 225, 135, 200,    100,    0, "Example", (255, 255, 255))
+        pygame.display.flip()
+        return button
---
+++ main.py
@@ -0,0 +1,33 @@
+import time
+import pygame
+from pygame.constants import MOUSEBUTTONDOWN
+from FrameBuffer import PyScope
+import xml.etree.ElementTree as ET
+
+__author__ = 'jacko'
+
+
+class ReadConfiguration(object):
+    def __init__(self, conf_file):
+        tree = ET.parse(conf_file)
+        root = tree.getroot()
+        for t in root.findall('type'):
+            name = t.get('name')
+            image = t.find('file').text
+            print name, image
+
+    def create_gui(self):
+        scope = PyScope()
+        button = scope.create_button()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                elif event.type == MOUSEBUTTONDOWN:
+                    if button.pressed(pygame.mouse.get_pos()):
+                        return
+
+
+conf = ReadConfiguration("conf.xml")
+conf.create_gui()
+#scope.display_image()
